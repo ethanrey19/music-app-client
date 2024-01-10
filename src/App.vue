60a3480e6d678f608205ea64c395cc4984a4145d
Ethan <ebud6805@gmail.com>
2024-01-09 23:39:35 -0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import Audio from './components/Audio.vue'
+</script>
+
+<template>
+  <div>
+    <Audio></Audio>
+  </div>
+  
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/Audio.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+      <button @click="playAudio">Play Audio</button>
+      <audio ref="audioPlayer" controls></audio>
+    </div>
+   </template>
+   
+   <script>
+   import axios from 'axios';
+   
+   export default {
+    methods: {
+      async playAudio() {
+        try {
+          const response = await axios.get('http://localhost:8080/api/song/1', { responseType: 'blob' });
+          const url = window.URL.createObjectURL(new Blob([response.data]));
+          this.$refs.audioPlayer.src = url;
+        } catch (error) {
+          console.error('Error:', error);
+        }
+      },
+    },
+   };
+   </script>
+   
